73b9cd2167b90dbc6078a68b532182044fbe4042
David Keegan <git@davidkeegan.com>
2012-03-21 00:56:25 -0700

    initial commit

--- PaintCodeExample/PCAppDelegate.m
+++ PaintCodeExample/PCAppDelegate.m
@@ -3,7 +3,7 @@
 //  PaintCodeExample
 //
 //  Created by David Keegan on 3/21/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 David Keegan. All rights reserved.
 //
 
 #import "PCAppDelegate.h"
--- PaintCodeExample/PCViewController.h
+++ PaintCodeExample/PCViewController.h
@@ -3,11 +3,13 @@
 //  PaintCodeExample
 //
 //  Created by David Keegan on 3/21/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 David Keegan. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface PCViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *debutsButton;
 
 @end
